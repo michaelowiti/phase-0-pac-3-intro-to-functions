a5fae56a045b72bdd42ba9c23ee1f06736de6b52
mike <mjowiti@gmail.com>
2023-06-08 15:23:15 +0300

    trying functions completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+
+  function sayHelloTo(firstName) {
+    console.log(`Hello,${firstName}!`);
+  }
+  sayHelloTo("joeBitch");
+  sayHelloTo("lilbBy");
+  sayHelloTo("rogirl")
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Julio", "hello");
+
+function add(x, y) {
+   
+    
+     return x+y;
+    
+}
+console.log(add(1,2));
+
+
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Sofia"));
+
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  console.log(say("Hello", "Sofia"));
